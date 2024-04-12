360dcdf4b97e49cca202d22b35dd8aa01e3b8a62
sawadyecma <snowslidez@yahoo.co.jp>
2024-04-12 17:53:05 +1000

    feat(makeRanking): support timer

--- src/reduce/makeRanking.ts
+++ src/reduce/makeRanking.ts
@@ -5,6 +5,7 @@ export function makeRanking(
   entries: Entry[]
 ): { order: number; count: number; url: string }[] {
   const counter = new Map<string, number>();
+  const timer = new Map<string, number>();
 
   entries.forEach((entry) => {
     const url = new URL(entry.request.url);
@@ -12,9 +13,18 @@ export function makeRanking(
 
     const count = counter.get(dynamicUrl);
     counter.set(dynamicUrl, count ? count + 1 : 1);
+
+    const newCount = counter.get(dynamicUrl)!;
+
+    const time = timer.get(dynamicUrl);
+    timer.set(
+      dynamicUrl,
+      time ? (time * (newCount - 1) + entry.time) / newCount : entry.time
+    );
   });
 
   console.log(Object.fromEntries(counter));
+  console.log(Object.fromEntries(timer));
 
   return [];
 }
